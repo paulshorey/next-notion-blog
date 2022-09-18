fd348d7c61636ea0b9632517a261f807342c3567
Paul Shorey <info@artspaces.net>
2022-09-18 15:09:15 -0500

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,97 @@
+// global styles shared across the entire site
+import 'styles/global.css'
+
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-coy.css'
+
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+
+// global style overrides for notion
+import 'styles/notion.css'
+
+// global style overrides for prism theme (optional)
+import 'styles/prism-theme.css'
+
+import 'styles/custom.css'
+
+import * as React from 'react'
+import Script from 'next/script'
+import * as Fathom from 'fathom-client'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import posthog from 'posthog-js'
+
+import { bootstrap } from 'lib/bootstrap-client'
+import {
+  isServer,
+  fathomId,
+  fathomConfig,
+  posthogId,
+  posthogConfig,
+  googleAnalyticsID,
+} from 'lib/config'
+
+if (!isServer) {
+  bootstrap()
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  React.useEffect(() => {
+    function onRouteChangeComplete() {
+      if (fathomId) {
+        Fathom.trackPageview()
+      }
+
+      if (posthogId) {
+        posthog.capture('$pageview')
+      }
+    }
+
+    if (fathomId) {
+      Fathom.load(fathomId, fathomConfig)
+    }
+
+    if (posthogId) {
+      posthog.init(posthogId, posthogConfig)
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router.events])
+
+  // Google Analytics support.
+  return (
+    <>
+      {
+        googleAnalyticsID && <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${googleAnalyticsID}');
+        `}
+          </Script>
+        </>
+      }
+      < Component {...pageProps} />
+    </>
+  )
+}
---
+++ pages/feed.xml.ts
@@ -0,0 +1,84 @@
+import { GetServerSideProps } from 'next'
+import RSS from 'rss';
+
+import { site, host, name, description, author } from '../lib/config';
+import { getSiteMap } from '../lib/get-site-map'
+import * as types from 'lib/types'
+import { uuidToId } from 'notion-utils';
+import { getCanonicalPageUrl } from 'lib/map-page-url'
+import { getSocialImageUrl } from 'lib/get-social-image-url'
+import { ExtendedRecordMap } from 'lib/types';
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+
+  if (req.method !== 'GET') {
+    res.statusCode = 405
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify({ error: 'method not allowed' }))
+    res.end()
+    return {
+      props: {}
+    }
+  }
+
+  const siteMap = await getSiteMap()
+
+  // cache for up to 8 hours
+  res.setHeader(
+    'Cache-Control',
+    'public, max-age=28800, stale-while-revalidate=28800'
+  )
+  res.setHeader('Content-Type', 'text/xml')
+
+  const feed = new RSS({
+    title: name,
+    site_url: host,
+    feed_url: `${host}/feed.xml`,
+    description,
+    copyright: `${new Date().getFullYear()} ${author}`,
+    webMaster: author,
+  })
+
+  // For each siteMap, add all the posts to the feed.
+  const pageMap = siteMap.canonicalPageMap
+  Object.keys(pageMap).map(pageURL => {
+    const pageData = pageMap[pageURL] as types.CanonicalPageData
+
+    // Skip the root page.
+    if (uuidToId(pageData.pageId) === siteMap.site.rootNotionPageId) {
+      return
+    }
+
+    // Skip the draft page.
+    if (pageData.publicPage == false) {
+      return
+    }
+
+    const recordMap = siteMap.pageMap[pageData.pageId] as ExtendedRecordMap
+    const url = getCanonicalPageUrl(site, recordMap)(pageData.pageId)
+    const socialImageUrl = getSocialImageUrl(pageData.pageId)
+    feed.item({
+      title: pageData.title,
+      // description: pageData.description,
+      url,
+      guid: pageData.pageId,
+      date: pageData.createdTime,
+      author,
+      enclosure: socialImageUrl
+        ? {
+          url: socialImageUrl,
+          type: 'image/jpeg'
+        }
+        : undefined
+    })
+  })
+
+  res.write(feed.xml({ indent: true }))
+
+  res.end()
+  return {
+    props: {}
+  }
+}
+
+export default () => null
